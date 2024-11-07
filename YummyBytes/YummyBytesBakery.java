036a4b3e69ab5c25d7c68f61303234a436465155
Asriel <sirawesome71@gmail.com>
2024-11-06 23:23:30 -0500

    11062024 changes
    -- a lot is broken rn
    -- changes to all the sell methods
    -- added printOrder methods
    -- modified the printOrderOption method

--- YummyBytes/YummyBytesBakery.java
+++ YummyBytes/YummyBytesBakery.java
@@ -20,8 +20,15 @@
  * __Added removeCookies(), removeDoughnuts(), removeMuffins()
  * __Updated printCookieCase() , printDoughnutCase() , printMuffinCase()
  * 
+ * 11062024
+ * __Added printOrderCookies(), printOrderMuffins(), printOrderDoughnuts()
+ * __Several changes to sellCookie(), sellMuffin(), sellDoughnut()
+ * __Changes to printOrderOption()
+ * 
+ * -- A LOT IS NOT WORKING RN --
+ * 
  * @author Gage R
- * @version 10312024.2
+ * @version 11062024
  */
 public class YummyBytesBakery
 {
@@ -42,7 +49,11 @@ public class YummyBytesBakery
     private Muffin[][] muffinCase;
     private Doughnut[][] doughnutCase;
 
+    //the scanner
     private java.util.Scanner reader;
+
+    //bool validation thing i use in like half my code lol
+    boolean valid;
 
     //default constructor
     YummyBytesBakery()
@@ -86,17 +97,23 @@ public class YummyBytesBakery
      * if you give too little money it won't give baked goods ;w;
      * error checks responses 
      * allows multiple purchases in a row
+     * checks if there is enough of inputed type of cookie in the case for the purchase
+     * removes cookies from the case with each purchase
+     * 
+     * -- DOES NOT CURRENTLY WORK , checkCookieCaseQuantity does not read the inputed string for some reason. 
+     * -- Make sure to remove the 'or' statement in the if elses when you figure it out on this one and the other sell methods
      */
     public void sellCookies()
     {
         double i;
         int amount;
-        boolean wantCookie;
         boolean didPay;
-        boolean valid;
 
-        do{
-            wantCookie = false;
+        System.out.println("What type of cookie would you like?");
+        String type = reader.nextLine();
+
+        if(checkCookieCaseQuantity(type) < 0 || 10 == 10)
+        {
             do
             {
                 valid = false;
@@ -105,7 +122,14 @@ public class YummyBytesBakery
                 reader.nextLine();
                 if (amount > 0)
                 {
-                    valid = true;
+                    if (checkCookieCaseQuantity(type) >= amount || 10 >= amount)
+                    {
+                        valid = true;
+                    }
+                    else
+                    {
+                        System.out.println("Sorry we don't have that many cookies right now");
+                    }
                 }
                 else
                 {
@@ -124,6 +148,7 @@ public class YummyBytesBakery
                 {
                     System.out.println("Thanks for your money, enjoy your cookie.");
                     register += i;
+                    removeCookies(amount, "Chocolate Chip");
                     didPay = true;
                 }
                 else
@@ -131,26 +156,11 @@ public class YummyBytesBakery
                     System.out.println("You have to pay you know");
                 }
             }while(didPay == false);
-
-            System.out.println("Would you like some more cookies? (y/n)");
-            do{
-                String response = reader.nextLine();
-                valid = true;
-                if(response.substring(0,1).equals("y"))
-                {
-                    wantCookie = true;
-                }
-                else if(response.substring(0,1).equals("n"))
-                {
-                    wantCookie = false;
-                }
-                else
-                {
-                    System.out.println("Enter a valid answer");
-                    valid = false;
-                }
-            }while(valid == false);
-        }while(wantCookie == true);
+        }
+        else
+        {
+            System.out.println("Sorry we dont have that cookie, please try again");
+        }
     }
 
     /**
@@ -159,17 +169,22 @@ public class YummyBytesBakery
      * allows multiple purchases in a row
      * if you give too little money it won't give baked goods ;w;
      * does a bunch of error checking
+     * checks if there is enough of inputed type of cookie in the case for the purchase
+     * removes cookies from the case with each purchase
+     * 
+     * -- ALSO DOESN'T WORK
      */
     public void sellMuffins()
     {
         double i;
         int amount;
-        boolean wantMuffin;
         boolean didPay;
-        boolean valid;
 
-        do{
-            wantMuffin = false;
+        System.out.println("What type of muffin would you like?");
+        String type = reader.nextLine();
+
+        if(checkMuffinCaseQuantity(type) < 0 || 10 == 10)
+        {
             do
             {
                 valid = false;
@@ -178,7 +193,14 @@ public class YummyBytesBakery
                 reader.nextLine();
                 if (amount > 0)
                 {
-                    valid = true;
+                    if (checkMuffinCaseQuantity(type) >= amount || 10 >= amount)
+                    {
+                        valid = true;
+                    }
+                    else
+                    {
+                        System.out.println("Sorry we don't have that many muffins right now");
+                    }
                 }
                 else
                 {
@@ -195,8 +217,9 @@ public class YummyBytesBakery
                 reader.nextLine();
                 if (cusPay >= i)
                 {
-                    System.out.println("Thanks for your money, enjoy your moofin.");
+                    System.out.println("Thanks for your money, enjoy your muffin.");
                     register += i;
+                    removeMuffins(amount, type);
                     didPay = true;
                 }
                 else
@@ -204,26 +227,11 @@ public class YummyBytesBakery
                     System.out.println("You have to pay you know");
                 }
             }while(didPay == false);
-
-            System.out.println("Would you like some more muffins? (y/n)");
-            do{
-                String response = reader.nextLine();
-                valid = true;
-                if(response.substring(0,1).equals("y"))
-                {
-                    wantMuffin = true;
-                }
-                else if(response.substring(0,1).equals("n"))
-                {
-                    wantMuffin = false;
-                }
-                else
-                {
-                    System.out.println("Enter a valid answer");
-                    valid = false;
-                }
-            }while(valid == false);
-        }while(wantMuffin == true);
+        }
+        else
+        {
+            System.out.println("Sorry we dont have that muffin, please try again");
+        }
     }
 
     /**
@@ -231,17 +239,22 @@ public class YummyBytesBakery
      * allows multiple purchases in a row
      * if you give too little money it won't give baked goods ;w;
      * adds money to the register with each purchase
+     * checks if there is enough of inputed type of cookie in the case for the purchase
+     * removes cookies from the case with each purchase
+     * 
+     * -- AGAIN DOES NOT CURRENTLY WORK
      */
     public void sellDoughnuts()
     {
         double i;
         int amount;
-        boolean wantDoughnut;
         boolean didPay;
-        boolean valid;
 
-        do{
-            wantDoughnut = false;
+        System.out.println("What type of doughnut would you like?");
+        String type = reader.nextLine();
+
+        if(checkDoughnutCaseQuantity(type) < 0 || 10 == 10)
+        {
             do
             {
                 valid = false;
@@ -250,7 +263,14 @@ public class YummyBytesBakery
                 reader.nextLine();
                 if (amount > 0)
                 {
-                    valid = true;
+                    if (checkDoughnutCaseQuantity(type) >= amount || 10 >= amount)
+                    {
+                        valid = true;
+                    }
+                    else
+                    {
+                        System.out.println("Sorry we don't have that many doughnuts right now");
+                    }
                 }
                 else
                 {
@@ -267,8 +287,9 @@ public class YummyBytesBakery
                 reader.nextLine();
                 if (cusPay >= i)
                 {
-                    System.out.println("Thanks for your money, enjoy your doughnut.");
+                    System.out.println("Thanks for your money, enjoy your doughnuts.");
                     register += i;
+                    removeDoughnuts(amount, type);
                     didPay = true;
                 }
                 else
@@ -276,30 +297,16 @@ public class YummyBytesBakery
                     System.out.println("You have to pay you know");
                 }
             }while(didPay == false);
-
-            System.out.println("Would you like some more doughnuts? (y/n)");
-            do{
-                String response = reader.nextLine();
-                valid = true;
-                if(response.substring(0,1).equals("y"))
-                {
-                    wantDoughnut = true;
-                }
-                else if(response.substring(0,1).equals("n"))
-                {
-                    wantDoughnut = false;
-                }
-                else
-                {
-                    System.out.println("Enter a valid answer");
-                    valid = false;
-                }
-            }while(valid == false);
-        }while(wantDoughnut == true);
+        }
+        else
+        {
+            System.out.println("Sorry we dont have that doughnut, please try again");
+        }
     }
 
     /**
      * Displays the order options in the terminal
+     * Lets user pick one of the pastry options then runs the printOrder for that pastery 
      */
     public void printOrderOption()
     {
@@ -307,6 +314,26 @@ public class YummyBytesBakery
         System.out.println("    Cookie");
         System.out.println("    Doughnut");
         System.out.println("    Muffin");
+
+        valid = true;
+        do{
+            String order = reader.nextLine();
+            switch(order.toLowerCase())
+            {
+                case "cookie":
+                    printOrderCookies();
+                    break;
+                case "doughnut":
+                    printOrderDoughnuts();
+                    break;
+                case "muffin":
+                    printOrderMuffins();
+                    break;
+                default:
+                    System.out.println("Sorry, please try again");
+                    valid = false;
+            }
+        }while(valid == false);
     }
 
     /**
@@ -442,7 +469,7 @@ public class YummyBytesBakery
             }   
         }
     }
-    
+
     /**
      * Displays what all doughnuts are in the case
      * 
@@ -518,7 +545,7 @@ public class YummyBytesBakery
      * 
      * @param cookieName Represents the name of the cookie you want to know the quantity of
      */
-    public void checkCookieCaseQuantity(String cookieName)
+    public int checkCookieCaseQuantity(String cookieName)
     {
         int quantity = 0;
         for(int r = 0; r < cookieCase.length; r++)
@@ -532,6 +559,7 @@ public class YummyBytesBakery
             }
         }
         System.out.println(quantity);
+        return quantity;
     }
 
     /**
@@ -539,7 +567,7 @@ public class YummyBytesBakery
      * 
      * @param doughnutName Represents the name of the doughnut you want to know the quantity of
      */
-    public void checkDoughnutCaseQuantity(String doughnutName)
+    public int checkDoughnutCaseQuantity(String doughnutName)
     {
         int quantity = 0;
         for(int r = 0; r < doughnutCase.length; r++)
@@ -553,6 +581,7 @@ public class YummyBytesBakery
             }
         }
         System.out.println(quantity);
+        return quantity;
     }
 
     /**
@@ -560,7 +589,7 @@ public class YummyBytesBakery
      * 
      * @param muffinName Represents the name of the muffin you want to know the quantity of
      */
-    public void checkMuffinCaseQuantity(String muffinName)
+    public int checkMuffinCaseQuantity(String muffinName)
     {
         int quantity = 0;
         for(int r = 0; r < muffinCase.length; r++)
@@ -574,8 +603,9 @@ public class YummyBytesBakery
             }
         }
         System.out.println(quantity);
+        return quantity;
     }
-    
+
     /**
      * Removes a quantity and type of cookie inputted by the user from the cookieCase array
      * 
@@ -596,7 +626,7 @@ public class YummyBytesBakery
             }
         }
     }
-    
+
     /**
      * Removes a quantity and type of dougnut inputted by the user from the doughnutCase array
      * 
@@ -617,7 +647,7 @@ public class YummyBytesBakery
             }
         }
     }
-    
+
     /**
      * Removes a quantity and type of cookie inputted by the user from the muffinCase array
      * 
@@ -637,5 +667,101 @@ public class YummyBytesBakery
                 }
             }
         }
+    }
+
+    /**
+     * Prints the cookie case out and then goes to sell cookies
+     * User is then prompted if they want to buy more cookies which will repeat the loop
+     */
+    public void printOrderCookies()
+    {
+        boolean wantCookie = false;
+        do{
+            printCookieCase();
+            sellCookies();
+
+            System.out.println("Would you like some more cookies? (y/n)");
+            do{
+                String response = reader.nextLine();
+                valid = true;
+                if(response.substring(0,1).equals("y"))
+                {
+                    wantCookie = true;
+                }
+                else if(response.substring(0,1).equals("n"))
+                {
+                    wantCookie = false;
+                }
+                else
+                {
+                    System.out.println("Enter a valid answer");
+                    valid = false;
+                }
+            }while(valid == false);
+        }while(wantCookie == true);
+    }
+
+    /**
+     * Prints out the muffin case and then goes to the sellMuffins method
+     * asks if the user would like to buy more and if they say yes it loops
+     */
+    public void printOrderMuffins()
+    {
+        boolean wantMuffin = false;
+        do{
+            printMuffinCase();
+            sellMuffins();
+
+            System.out.println("Would you like some more muffins? (y/n)");
+            do{
+                String response = reader.nextLine();
+                valid = true;
+                if(response.substring(0,1).equals("y"))
+                {
+                    wantMuffin = true;
+                }
+                else if(response.substring(0,1).equals("n"))
+                {
+                    wantMuffin = false;
+                }
+                else
+                {
+                    System.out.println("Enter a valid answer");
+                    valid = false;
+                }
+            }while(valid == false);
+        }while(wantMuffin == true);
+    }
+
+    /**
+     * Prints out the muffin case and then goes to the sellDoughnuts method
+     * asks if the user would like to buy more and if they say yes it loops
+     */
+    public void printOrderDoughnuts()
+    {
+        boolean wantDoughnut = false;
+        do{
+            printDoughnutCase();
+            sellDoughnuts();
+
+            System.out.println("Would you like some more doughnuts? (y/n)");
+            do{
+                String response = reader.nextLine();
+                valid = true;
+                if(response.substring(0,1).equals("y"))
+                {
+                    wantDoughnut = true;
+                }
+                else if(response.substring(0,1).equals("n"))
+                {
+                    wantDoughnut = false;
+                }
+                else
+                {
+                    System.out.println("Enter a valid answer");
+                    valid = false;
+                }
+            }while(valid == false);
+        }while(wantDoughnut == true);
     }
 }
